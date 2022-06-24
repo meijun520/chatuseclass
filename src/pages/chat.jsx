6737fa57ac7434658212138ed0ecc3ca912e1177
xmj520 <510504307@qq.com>
2022-06-24 14:26:08 +0800

    修改问题

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -7,21 +7,19 @@ let io = socketio('http://localhost:3000', {
   transports: ['websocket'],
 });
 export default class WeChat extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       list: [],
-      message:'',
-      user:'',
-      count:0
+      message: '',
+      user: '',
+      count: 0,
     };
   }
   componentDidMount() {
-    this.setState({user:history.location.query.user});
- 
-    console.log(io.id,999);
-    io.emit('user', this.state.user);
-    console.log(this.state.user, 8989);
+    this.setState({ user: history.location.query.user }, () => {
+      io.emit('user', this.state.user);
+    });
     io.on('islogin', (msg) => {
       if (msg.islogin === false && msg.user === this.state.user) {
         io.close();
@@ -29,12 +27,12 @@ export default class WeChat extends React.Component {
         history.push('/');
       }
     });
-    io.on('pushMsg',(msg)=>{
+    io.on('pushMsg', (msg) => {
       this.state.list.push(msg);
-      this.setState({list:[...this.state.list]});
+      this.setState({ list: [...this.state.list] });
     });
-    io.on('countmsg',  (msg)=> {
-      this.setState({count:msg});
+    io.on('countmsg', (msg) => {
+      this.setState({ count: msg });
     });
   }
   // componentWillReceiveProps (nextProps){
@@ -51,45 +49,66 @@ export default class WeChat extends React.Component {
   //   }
   // };
 
- sendmessage() {
+  sendmessage() {
     if (this.state.message) {
-      io.emit('sendMsg', { name: this.state.user, message:this.state.message });
-      this.setState({message:''});
+      io.emit('sendMsg', {
+        name: this.state.user,
+        message: this.state.message,
+      });
+      this.setState({ message: '' });
+    }
+  }
+  keyup(e) {
+    if (e.keyCode == 13 && e.target.value) {
+      io.emit('sendMsg', {
+        name: this.state.user,
+        message: this.state.message,
+      });
+      this.setState({ message: '' });
     }
   }
 
-  render(){
-    return  <div className={style.chat}>
-      <div>
-        在线人数{this.state.count}
-        {history.location.query.user}
-      </div>
-      <div className={style.right}>
-        <ul className={style.messages} id="mes">
-          {this.state.list.map((item, index) => (
-            <li
-              key={index}
-              className={
-                this.state.user == item.name ? style.chartlistright : style.chartlistleft
+  render() {
+    return (
+      <div className={style.chat}>
+        <div>
+          在线人数{this.state.count}
+          {history.location.query.user}
+        </div>
+        <div className={style.right}>
+          <ul className={style.messages} id="mes">
+            {this.state.list.map((item, index) => (
+              <li
+                key={index}
+                className={
+                  this.state.user == item.name
+                    ? style.chartlistright
+                    : style.chartlistleft
+                }
+              >
+                {this.state.user == item.name
+                  ? item.message + item.name + ':'
+                  : item.name + ':' + item.message}
+              </li>
+            ))}
+          </ul>
+          <div className={style.form}>
+            <input
+              type="text"
+              className={style.m}
+              id="input"
+              value={this.state.message}
+              onKeyUp={(e) => {
+                this.keyup(e);
+              }}
+              onChange={(event) =>
+                this.setState({ message: event.target.value })
               }
-            >
-              {this.state.user == item.name
-                ? item.message + item.name + ':'
-                : item.name + ':' + item.message}
-            </li>
-          ))}
-        </ul>
-        <div className={style.form}>
-          <input
-            type="text"
-            className={style.m}
-            id="input"
-            value={this.state.message}
-            onChange={(event) => this.setState({message:event.target.value})}
-          />
-          <button onClick={()=>this.sendmessage()}>send</button>
+            />
+            <button onClick={() => this.sendmessage()}>send</button>
+          </div>
         </div>
       </div>
-    </div>
+    );
   }
 }
